319150057b29c46019ac76c1ad91f8ad7552f1d8
Kauê Pires Moreira <kauepiresmoreira09@gmail.com>
2026-04-24 20:35:21 -0300

    feat: Criação de Interface PedidoRepository puxando dados do Pedido

--- src/main/java/br/com/fiap3ESS/checkpoint2/repository/PedidoRepository.java
+++ src/main/java/br/com/fiap3ESS/checkpoint2/repository/PedidoRepository.java
@@ -1,4 +1,8 @@
 package br.com.fiap3ESS.checkpoint2.repository;
 
-public interface PedidoRepository {
+import br.com.fiap3ESS.checkpoint2.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
 }
