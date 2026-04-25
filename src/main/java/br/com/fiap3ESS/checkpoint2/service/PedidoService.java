5f359b2501272f3ee67fc18469364d4d7b2a910e
Kauê Pires Moreira <kauepiresmoreira09@gmail.com>
2026-04-24 21:02:05 -0300

    feat: Criação de regra de negocio com @Service

--- src/main/java/br/com/fiap3ESS/checkpoint2/service/PedidoService.java
+++ src/main/java/br/com/fiap3ESS/checkpoint2/service/PedidoService.java
@@ -1,4 +1,32 @@
 package br.com.fiap3ESS.checkpoint2.service;
 
+import br.com.fiap3ESS.checkpoint2.model.Pedido;
+import br.com.fiap3ESS.checkpoint2.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public List<Pedido> listarTodos() {
+        return pedidoRepository.findAll();
+    }
+
+    public Optional<Pedido> buscarPorId(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public Pedido salvar(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
+
+    public void deletar(Long id) {
+        pedidoRepository.deleteById(id);
+    }
+
 }
